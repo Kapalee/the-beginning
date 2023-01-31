f0d8083b7d48918818e467972c52b8f4a136befb
Kapalee <Shamvictor@hotmail.com>
2023-01-31 12:33:41 +0100

    Mine første ændringer
    Jeg har ændret overskrift, fonten på skriften, lavet en pop-up alarm, lavet teskt i DevTools og ændret baggrunden på HTML

---
+++ app.js
@@ -0,0 +1,6 @@
+document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "Hello JavaScript!";
+// alert("THE RUSSIANS ARE COMING");
+console.log("Hello Dat.js");
+console.log("Hvaså kammerat båtnakke");
+console.log("Jeg hedder henning og jeg elsker dressing");
